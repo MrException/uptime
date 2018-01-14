2dbaa5353121efa8533f61e732b53da3a14a162e
Robert McBride <robmcbride@gmail.com>
2018-01-13 23:13:09 -0800

    add docker-compose to provision

--- provision.sh
+++ provision.sh
@@ -13,4 +13,7 @@ sudo yum install -y nodejs
 curl --silent --location https://get.docker.com | sudo bash -
 sudo usermod -aG docker vagrant
 
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 sudo npm install -g serverless
